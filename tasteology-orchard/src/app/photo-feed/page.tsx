1319a721577abb412b7f2b9b5dce21c5fcd3ef9f
marvin-aoanan <cybro.vin@gmail.com>
2025-03-30 01:35:30 +0800

    fix eslint errors

--- tasteology-orchard/src/app/photo-feed/page.tsx
+++ tasteology-orchard/src/app/photo-feed/page.tsx
@@ -1,8 +1,5 @@
-import Link from "next/link";
 import BottomComponent from "../components/bottom";
 import TopComponent from "../components/top";
-import photos from "./photos";
-import Image from "next/image";
 
 export default function Home() {
   return (
